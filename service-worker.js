add0a996b510c576918ece50c9ad5e4a9cdc3a8f
regat <regatpeter@hotmail.fr>
2026-08-07 00:45:21 +0200

    fix: normalize partner mission planning date

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "depann-home-pro-v270";
+const CACHE_NAME = "depann-home-pro-v271";
 const ASSETS = [
     "./",
     "./index.html",
     "./css/style.css",
     "./css/report-editor.css?v=4",
-    "./js/app.js?v=254",
+    "./js/app.js?v=255",
     "./js/accounting.js",
     "./js/accounting-sandbox.js",
     "./js/groups.js",
