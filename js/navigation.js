87240234a571fc3322fe246b4e440c679c80d201
regat <regatpeter@hotmail.fr>
2026-08-08 15:49:56 +0200

    feat: add journal visibility checkboxes

--- js/navigation.js
+++ js/navigation.js
@@ -5,7 +5,7 @@ import { createBillingDocumentForClient, renderBilling, synchronizeBillingDocume
 import { renderAccounting } from "./accounting.js?v=5";
 import { renderAccountingSandbox } from "./accounting-sandbox.js?v=2";
 import { renderGroupActivation, renderGroupWorkspace } from "./groups.js?v=3";
-import { renderPartnerMissions } from "./partner-missions.js?v=26";
+import { renderPartnerMissions } from "./partner-missions.js?v=27";
 import { renderPartnerSandbox } from "./partner-sandbox.js?v=3";
 import { renderPartnerConnections } from "./partner-connections.js?v=13";
 import { renderDataImportTool } from "./data-imports.js?v=2";
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "depann-home-pro-v283";
+const CACHE_NAME = "depann-home-pro-v284";
 const ASSETS = [
     "./",
     "./index.html",
